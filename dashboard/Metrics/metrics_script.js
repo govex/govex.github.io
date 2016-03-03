7d11e4ee4b3f1ae0480f140ba2bd9ccb6ca822df
Nick Hadjigeorge <nghadjigeorge@gmail.com>
2016-03-03 14:32:33 -0500

    update

--- dashboard/Metrics/metrics_script.js
+++ dashboard/Metrics/metrics_script.js
@@ -49,8 +49,34 @@ url_array.forEach(function(myUrl){
         var dataTextObj = JSON.stringify(dataObj);
         $('#displayData').text(dataTextObj);
 
+        var json = dataObj;
+        var fields = Object.keys(json[0]);
+        var csv = json.map(function(row){
+          return fields.map(function(fieldName){
+            return '"' + (row[fieldName] || '') + '"';
+          });
+        });
+        csv.unshift(fields); // add header column
+
+        console.log(csv.join('\r\n'));
+
       }).fail(function(xhr, status, err) {
           console.log('fail', status, err);
           });
         });
+
+
+        var json = dataTextObj;
+        var fields = Object.keys(json[0]);
+        var csv = json.map(function(row){
+          return fields.map(function(fieldName){
+            return '"' + (row[fieldName] || '') + '"';
+          });
+        });
+        csv.unshift(fields); // add header column
+
+        console.log(csv.join('\r\n'));
+
+        
+
 });
