db2fc698c7be95d54da7f1d61457dcc4bd12f668
Matt Pazoles <pazoles@gmail.com>
2016-05-25 13:09:06 -0400

    Update page-shield.js

--- maps/page-shield.js
+++ maps/page-shield.js
@@ -17,7 +17,7 @@ pageShieldContent.style.color = "white";
 pageShieldContent.style.textAlign = "center";
 pageShieldContent.style.verticalAlign = "middle";
 pageShieldContent.style.display = "table-cell";
-pageShieldContent.style.zindex = "1000000";
+pageShieldContent.style.zIndex = "2147483647";
 
 function injectShield() {
     if (document.body) {
